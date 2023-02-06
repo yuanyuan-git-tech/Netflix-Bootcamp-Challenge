a9b778981dce923bcf68db0995efe3183dd2cf32
Yuanyuan Zhao <qsn7pu@virginia.edu>
2023-02-05 22:12:28 -0500

    completed the tests

--- Summative-1-Yuanyuan-Zhao/src/test/java/com/company/Summative1YuanyuanZhao/controller/ApiControllerTest.java
+++ Summative-1-Yuanyuan-Zhao/src/test/java/com/company/Summative1YuanyuanZhao/controller/ApiControllerTest.java
@@ -9,6 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.http.MediaType;
+
 
 import java.util.List;
 
@@ -25,24 +30,32 @@ public class ApiControllerTest {
     private ObjectMapper mapper = new ObjectMapper();
 
     // A list of records for testing purposes
-    private List<Answer> answerList;
 
-    @Before
-    public void setUp() {
-
-    }
 
     @Test
-    public void getQuote() {
-
+    public void getQuote() throws Exception {
+        mockMvc.perform(get("/quote")).andExpect(status().isOk());
     }
     @Test
-    public void getWord() {
-
+    public void getWord() throws Exception {
+        mockMvc.perform(get("/quote")).andExpect(status().isOk());
     }
     @Test
-    public void postAnswer() {
+    public void postAnswer() throws Exception {
+        // ARRANGE
+        Answer inputAnswer = new Answer();
+        inputAnswer.setAnswer("I am good.");
+        inputAnswer.setQuestion("How are you?");
 
+        // Convert Java Object to JSON
+        String inputJson = mapper.writeValueAsString(inputAnswer);
+        mockMvc.perform(
+                post("/magic")
+                        .content(inputJson)                       // Set the request body
+                        .contentType(MediaType.APPLICATION_JSON)  // Tell the server it's in JSON format
+                )
+                .andDo(print())                // Print results to console
+                .andExpect(status().isCreated());        // ASSERT (status code is 201)
     }
 
 }
